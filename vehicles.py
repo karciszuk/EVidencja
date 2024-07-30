4a2011b5c03aee2f6003dc6ce2eea10e8575b15a
karciszuk <karciszuk@gmail.com>
2024-07-31 00:36:15 +0200

    0.137

--- vehicles.py
+++ vehicles.py
@@ -22,47 +22,41 @@ date_difference = int(current_date[:4])-2010
 for klucz,wartosc in zip(df['klucz-slownika'],df['wartosc-slownika']):
         original_date = '20100101'
         for i in range(date_difference):
-                for l in range(1,12):
-                        k = 1
-                        match = 1
-                        while k <= match:
-                                if l < 10:
-                                        month = "0"+str(l)
-                                else:
-                                        month = str(l)
-                                start_date = str(int(original_date[:4]) + i)+month+"01"
-                                last_day = str(calendar.monthrange(int(start_date[:4]), int(month))[1])
-                                next_date = str(int(start_date[:4]))+month+last_day
-                                params={
-                                        "wojewodztwo": str(klucz),
-                                        "data-od": start_date,
-                                        'data-do': next_date,
-                                        "typ-daty": "1",
-                                        "tylko-zarejestrowane": "true",
-                                        "pokaz-wszystkie-pola": "false",
-                                        "limit": "500",
-                                        "page": str(k)
-                                        }
-                                new_file_name=wartosc+" "+start_date+"-"+next_date+".csv"
-                                files = check_file(path,new_file_name)   
-                                if files:
-                                        continue
-                                df, data = connect_and_dataload(api_link,params) 
-                                last_link = data['links']['last']
-                                match = int(re.search(r'page=(\d+)&typ-daty', last_link))
-                                if match:
-                                        print(match.group(1))
-                                else:
-                                        print("Pattern not found")
-                                df = pd.DataFrame(df)
-                                df.columns = df.columns.str.replace('attributes.','', regex=True)
-                                df.columns = df.columns.str.replace('-', '_', regex=True)
-                                df = df.drop(columns=['links.self'])
-
-                                to_csv(df,path+new_file_name)
-
-                                time.sleep(0.7)
-
-                                k += 1
+                k = 1
+                match = 2
+                for k in range(1, match):
+                        print(k)
+                        start_date = str(int(original_date[:4]) + i)+"0101"
+                        print(start_date)
+                        next_date = str(int(start_date[:4])+1)+"1231"
+                        print(next_date)
+                        print(klucz)
+                        params={
+                                "wojewodztwo": str(klucz),
+                                "data-od": start_date,
+                                'data-do': next_date,
+                                "typ-daty": "1",
+                                "tylko-zarejestrowane": "true",
+                                "pokaz-wszystkie-pola": "false",
+                                "limit": "500",
+                                "page": str(k)
+                                }
+                        new_file_name=wartosc+" "+start_date+"-"+next_date+" page "+str(k)+".csv"
+                        files = check_file(path,new_file_name)   
+                        if files:
+                                continue
+                        df, data = connect_and_dataload(api_link,params) 
+                        last_link = data['links']['last']
+                        match = re.search(r'page=(\d+)&typ-daty', last_link)
+                        match = int(match.group(1))
+                        print(match)
+                        df = pd.DataFrame(df)
+                        df.columns = df.columns.str.replace('attributes.','', regex=True)
+                        df.columns = df.columns.str.replace('-', '_', regex=True)
+                        df = df.drop(columns=['links.self'])
+                        to_csv(df,path+new_file_name)
+                        k += 1
+                        print(k)
+                        time.sleep(0.7)
 
 print(f"Vehicles file '{new_file_name}' has been created.")
